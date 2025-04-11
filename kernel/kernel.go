e1c704f6e626484e7ed1e8a26dc31a8ad67aa2e9
Guido <gbassi@frba.utn.edu.ar>
2025-04-11 20:28:20 -0300

    se agrega conexion faltante
    Co-authored-by: Tomás Benjamín Pochat <tompochat@users.noreply.github.com>
    Co-authored-by: Fabri <jdesimone@frba.utn.edu.ar>
    Co-authored-by: Thiago Molina <tmolina@frba.utn.edu.ar>

--- kernel/kernel.go
+++ kernel/kernel.go
@@ -2,19 +2,28 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"utils"
 )
 
 func main() {
 	utils.ConfigurarLogger("log_KERNEL")
-	config := utils.CargarConfiguracion[utils.ConfigKernel]("config.json")
+	kernelConfig := utils.CargarConfiguracion[utils.ConfigKernel]("config.json")
+
+	if kernelConfig == nil {
+		log.Println("Error al cargar la configuracion de Kernel")
+		return
+	}
+
+	utils.EnviarMensaje(kernelConfig.IPMemory, kernelConfig.PortMemory,"peticiones","Hola desde Kernel")
+
 	mux := http.NewServeMux()
 
 	// mux.HandleFunc("/procesos", utils.RecibirPaquetes)
 	mux.HandleFunc("/interrupciones", utils.RecibirMensaje)
 
-	err := http.ListenAndServe(fmt.Sprintf(":%d",config.PortKernel), mux)
+	err := http.ListenAndServe(fmt.Sprintf(":%d",kernelConfig.PortKernel), mux)
 	if err != nil {
 		panic(err)
 	}
--- memoria/memoria.go
+++ memoria/memoria.go
@@ -11,7 +11,7 @@ func main() {
 	config := utils.CargarConfiguracion[utils.ConfigMemory]("config.json")
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/interrupciones", utils.RecibirMensaje)
+	mux.HandleFunc("/peticiones", utils.RecibirMensaje)
 
 	err := http.ListenAndServe(fmt.Sprintf(":%d",config.PortMemory), mux)
 	if err != nil {
