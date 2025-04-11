51af2cc87e13b6449d9df9ee0fbd904f9e69bece
Guido <gbassi@frba.utn.edu.ar>
2025-04-11 20:05:23 -0300

    cpu manda mensaje a kernel

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -2,9 +2,21 @@ package main
 
 import (
 	"utils"
+	"log"
 )
 
 func main() {
 	utils.ConfigurarLogger("log_CPU")
-	
+	CPUConfig := utils.CargarConfiguracion[utils.ConfigCPU]("config.json")
+
+	if CPUConfig == nil {
+		log.Println("Error al cargar la configuracion de CPU")
+		return
+	}
+
+	log.Printf("Configuracion de CPU cargada correctamente: %+v", CPUConfig)
+
+	utils.EnviarMensaje(CPUConfig.IPKernel, CPUConfig.PortKernel, "Hola desde CPU")
+
+
 }
