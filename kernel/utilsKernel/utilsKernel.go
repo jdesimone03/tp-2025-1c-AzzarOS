29890be08286bbfeb1d1f69ddb3d6fe465176b4e
Guido <gbassi@frba.utn.edu.ar>
2025-04-25 12:16:04 -0300

    Agrego funciones de prueba y acomodo (o emperoro) el planificador

--- kernel/utilsKernel/utilsKernel.go
+++ kernel/utilsKernel/utilsKernel.go
@@ -13,6 +13,7 @@ import (
 var Config = config.CargarConfiguracion[config.ConfigKernel]("config.json")
 
 var ColaNew []structs.PCB
+var ColaReady []structs.PCB
 
 // scheduler_algorithm: LARGO plazo
 // ready_ingress_algorithm: CORTO plazo
@@ -73,42 +74,76 @@ func addInterfaz(nombre string, ifaz structs.Interfaz) {
 	Interfaces[nombre] = ifaz
 }
 
-//func AdministrarColas(pcb structs.PCB) {
-//	switch pcb.Estado {
-//	case "NEW":
-//		utils.NuevoProceso(pcb)
-//		ColaNew = append(ColaNew, pcb)
-//	case "READY":
-//		//TODO: Agregar a la cola de READY
-//	case "EXEC":
-//		//TODO: Agregar a la cola de EXEC
-//	case "BLOCKED":
-//		//TODO: Agregar a la cola de BLOCKED
-//	case "EXIT":
-//		//TODO: Agregar a la cola de EXIT
-//	case "SUSP_BLOCKED":
-//		//TODO: Agregar a la cola de SUSP_BLOCKED
-//	case "SUSP_READY":
-//		//TODO: Agregar a la cola de SUSP_READY
-//	default:
-//		slog.Error(fmt.Sprintf("Estado de PCB no reconocido: %s", pcb.Estado))
-//	}
-//}
-//
-//func PlanificadorLargoPlazo(pcb structs.PCB) {
-//	if ColaNew == nil {
-//		//SE ENVIA PEDIDO A MEMORIA, SI ES OK SE MANDA A READY
-//		pcb.Estado = structs.EstadoReady
-//		AdministrarColas(pcb)
-//		
-//	}else {
-//		switch Config.SchedulerAlgorithm {
-//		case "FIFO":
-//			//ejecutar FIFO
-//		case "SJF":
-//			//ejecutar SJF
-//		default:
-//			slog.Error(fmt.Sprintf("Algoritmo de planificacion no reconocido: %s", Config.SchedulerAlgorithm))
-//		}
-//	}
-//}
+func AdministrarColas(pcb structs.PCB) {
+	switch pcb.Estado {
+	case "NEW":
+		utils.NuevoProceso(pcb)
+		ColaNew = append(ColaNew, pcb)
+	case "READY":
+		//TODO: Agregar a la cola de READY
+	case "EXEC":
+		//TODO: Agregar a la cola de EXEC
+	case "BLOCKED":
+		//TODO: Agregar a la cola de BLOCKED
+	case "EXIT":
+		//TODO: Agregar a la cola de EXIT
+	case "SUSP_BLOCKED":
+		//TODO: Agregar a la cola de SUSP_BLOCKED
+	case "SUSP_READY":
+		//TODO: Agregar a la cola de SUSP_READY
+	default:
+		slog.Error(fmt.Sprintf("Estado de PCB no reconocido: %s", pcb.Estado))
+	}
+}
+
+func PlanificadorLargoPlazo(pcb structs.PCB) {
+	if ColaNew == nil {
+		//SE ENVIA PEDIDO A MEMORIA, SI ES OK SE MANDA A READY
+		//ASUMIMOS EL CAMINO LINDO POR QUE NO ESTA HECHO LO DE MEMORIA
+		pcb.Estado = structs.EstadoReady
+		MoverPCB(pcb.PID, &ColaNew, &ColaReady)		
+		
+	}else {
+		switch Config.SchedulerAlgorithm {
+		case "FIFO":
+			//ejecutar FIFO
+		case "SJF":
+			//ejecutar SJF
+		default:
+			slog.Error(fmt.Sprintf("Algoritmo de planificacion no reconocido: %s", Config.SchedulerAlgorithm))
+		}
+	}
+}
+
+func MoverPCB(pid uint, origen *[]structs.PCB, destino *[]structs.PCB) {
+    for i, pcb := range *origen {
+        if pcb.PID == pid {
+            *destino = append(*destino, pcb) // mover a la cola destino
+            *origen = append((*origen)[:i], (*origen)[i+1:]...) // eliminar del origen
+            return
+        }
+    }
+}
+
+
+
+
+//---------------------------- Funciones de prueba ----------------------------//
+func NuevoProceso() structs.PCB {
+	var pcb = CrearPCB(1, 0, structs.EstadoNew)
+	ColaNew = append(ColaNew, pcb)
+	slog.Info(fmt.Sprintf("Se agregó el proceso %d a la cola de new", pcb.PID))
+	return pcb
+}
+
+func CrearPCB(pid uint,pc uint, estado string) structs.PCB {
+	slog.Info(fmt.Sprintf("Se ha creado el proceso %d", pid))
+	return structs.PCB{
+		PID:    pid,
+		PC:     pc,
+		Estado: estado,
+		MetricasConteo: nil,
+		MetricasTiempo: nil,
+	}
+}
+//-------------------------------------------------------------------------------//
